2077613eab197b45ccc151081719baddd41f9f47
pekko1215 <s16t218@stu.kagawa-u.ac.jp>
2018-03-02 17:08:05 +0900

    first commit

---
+++ generator/fairyMaker.js
@@ -0,0 +1,80 @@
+(function($) {
+    $.fn.wysiwygEvt = function() {
+        return this.each(function() {
+            var $this = $(this);
+            var htmlOld = $this.html();
+            $this.bind('blur keyup paste copy cut mouseup', function() {
+                var htmlNew = $this.html();
+                if (htmlOld !== htmlNew) {
+                    $this.trigger('change');
+                    htmlOld = htmlNew;
+                }
+            })
+        })
+    }
+})(jQuery);
+
+
+function* attriubteGenerator() {
+    const attriubtes = ['fire', 'thunder', 'water', 'light', 'dark']
+    var main, sub;
+    main = sub = 0;
+    while (true) {
+        yield [attriubtes[main], attriubtes[sub]];
+        sub++;
+        if (sub == attriubtes.length) {
+            sub = 0;
+            main++;
+        }
+        if (main == 3) {
+            main = 0;
+        }
+    }
+}
+
+var attrgen = attriubteGenerator();
+
+var nowAttr = attrgen.next().value;
+changeFrame(nowAttr)
+
+
+$('.card-attrs-skill-title').wysiwygEvt().on('change', (e) => {
+    var $el = $(e.target);
+    if ($el.text() === '') {
+        $el.text('なし')
+    }
+})
+
+$('.card-attr-changer').on('click', () => {
+    nowAttr = attrgen.next().value;
+    changeFrame(nowAttr)
+})
+
+$('#filer').on('change', (e) => {
+    var files = e.target.files;
+    var reader = new FileReader;
+
+    reader.readAsDataURL(files[0]);
+    reader.onload = function() {
+        var url = reader.result;
+        $('.card-image-source').attr({
+            src: url
+        })
+    }
+})
+
+$('#generate').click(e => {
+    html2canvas(document.getElementById('card')).then(function(canvas) {
+        var $canvas = $(canvas);
+        $('#output > canvas').remove();
+        $('#output').append($canvas);
+    });
+})
+
+function changeFrame(attriubte) {
+    var arr = $('.card-frame').attr('src').split('\/');
+    arr[arr.length - 1] = attriubte.join('_') + '.png'
+    $('.card-frame').attr({
+        src: arr.join('\/')
+    })
+}
